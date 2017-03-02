8850d3262b6935c5a3f0b04dc5641c788c950378
vigotskij <mdqulto@gmail.com>
2017-03-02 18:14:55 -0300

    testing building _incomplete_

---
+++ test/control.spec.js
@@ -0,0 +1,8 @@
+describe("Control", function() {
+  // aca van a ir los it( ...)
+  it("should create window.Control", function()
+    {
+      chai.expect(typeof window.Control).to.eql("object")
+    }
+  );
+});
