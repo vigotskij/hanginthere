b2c0138bc2a8d6b6ee020b8ae9ff2f43cad57e13
vigotskij <mdqulto@gmail.com>
2017-03-03 11:04:16 -0300

    trying to pass the tests

--- js/control.js
+++ js/control.js
@@ -1,1 +1,14 @@
-window.Control = { } ;
+window.Control = {
+  createEmptyBox :  function ()
+                    {
+                      document.createElement( div [ letter , letter-box ]) ;
+                    } ,
+  init :            function ( secretWord )
+                    {
+                      for ( let idx = 0 ; idx < secretWord.length ; idx++ )
+                      {
+                        window.Control.createEmptyBox( ) ;
+                      }
+                    } ,
+
+} ;
