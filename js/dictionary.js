10045fab6bed9c35a08d921195a5ac4ad5c29673
vigotskij <mdqulto@gmail.com>
2017-03-19 15:00:58 -0300

    Adding dictionary

---
+++ js/dictionary.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Diccionary = {
+
+/*randomWord :  function(){
+                const word = Diccionary[ Math.random().toInt ]
+
+                return word ;
+              } ,
+ */
+}
