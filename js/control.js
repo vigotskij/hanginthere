e66c1ecae12e64255857aef7615b7f0b7160ebb2
vigotskij <mdqulto@gmail.com>
2017-03-21 10:45:01 -0300

    inserting 'available' on Control

--- js/control.js
+++ js/control.js
@@ -6,8 +6,11 @@ var Control = {
   toWin :           0 ,
   matches :         [ ] ,
   errors :          [ ] ,
-  forbiden :        [ "1" , "2" , "3" , "4" ,"5" , "6" , "7" , "8" , "9" , "0" ,
-                      "" , "<" , ">"] ,
+  available :       [
+                      "a" , "b" , "c" , "d" ,"e" , "f" , "g" , "h" , "i" , "j" ,
+                      "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" ,
+                      "u" , "v" , "w" , "x" , "y" , "z"
+                    ] ,
 
   createEmptyBox :  function ()
                     {
@@ -65,7 +68,7 @@ var Control = {
                       this.gameOn() ;
                       return ;
                     } ,
-evtListener :       function ( evt )
+  evtListener :       function ( evt )
                     {
                       const input = String.fromCharCode( evt.charCode ).toLowerCase() ;
 
@@ -84,7 +87,7 @@ evtListener :       function ( evt )
                         ;
                       }
                     } ,
-gameOn :            function()
+  gameOn :            function()
                     {
                       let evtListener = function ( evt )
                       {
@@ -144,8 +147,8 @@ gameOn :            function()
                       let
                           inErrors  = this.errors.indexOf( input ) !== -1 ,
                           inMatches = this.matches.indexOf( input ) !== -1 ,
-                          inForbiden = this.forbiden.indexOf( input ) !== -1 ;
-                      return ( inErrors || inMatches || inForbiden ) ;
+                          inAvailable = this.available.indexOf( input ) === -1 ;
+                      return ( inErrors || inMatches || inAvailable ) ;
                     } ,
   monigoteDraw :    function()
                     {
